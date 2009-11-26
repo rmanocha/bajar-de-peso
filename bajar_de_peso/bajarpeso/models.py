7781516d679e3a9848ec2735664cf9f3f562937a
Rishabh Manocha <rmanocha@gmail.com>
2009-11-26 15:24:04 +0800

    Adding a string representation to the WeightTracker model and moving a few things around in the view.

--- bajar_de_peso/bajarpeso/models.py
+++ bajar_de_peso/bajarpeso/models.py
@@ -6,3 +6,6 @@ class WeightTracker(db.Model):
     user = db.UserProperty(required = True, auto_current_user_add = True)
     date = db.DateProperty(auto_now_add = True)
     weight = db.FloatProperty()
+
+    def __str__(self):
+        return '%skgs on %s' % (self.weight, self.date)
--- bajar_de_peso/bajarpeso/views.py
+++ bajar_de_peso/bajarpeso/views.py
@@ -1,8 +1,13 @@
 # Create your views here.
 
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 from bajar_de_peso.bajarpeso.models import WeightTracker
 
 def main(request):
-    return HttpResponse('Thanks')
+    if request.method == 'POST':
+        print "here"
+        return HttpResponse('Welcome!!')
+    else:
+        return HttpResponse('Thanks')
