9d104fbc4751ac65e11873f353c3910146944f0d
Rishabh Manocha <rmanocha@gmail.com>
2009-11-28 11:14:19 +0800

    Everything in this app will happen after a user has signed in, so using login: required (see http://code.google.com/appengine/docs/python/config/appconfig.html#Requiring_Login_or_Administrator_Status).
    Also adding a new model to store settings and making the date and weight fields in WeightTracker as required.

--- bajar_de_peso/bajarpeso/models.py
+++ bajar_de_peso/bajarpeso/models.py
@@ -1,12 +1,17 @@
 from google.appengine.ext import db
-from google.appengine.api import users
-
-# Create your models here.
 
 class WeightTracker(db.Model):
     user = db.UserProperty(required = True, auto_current_user_add = True)
-    date = db.DateProperty(auto_now_add = True)
-    weight = db.FloatProperty()
+    date = db.DateProperty(required = True, auto_now_add = True)
+    weight = db.FloatProperty(required = True)
 
     def __str__(self):
-        return '%skgs on %s for %s' % (self.weight, self.date, users.get_current_user().nickname())
+        return '%skgs on %s for %s' % (self.weight, self.date, self.user.nickname())
+
+class WeightTrackerSettings(db.Model):
+    user = db.UserProperty(required = True, auto_current_user_add = True)
+    units = db.StringProperty(required = True, choices = set(["lbs","kgs"]))
+
+    def __str__(self):
+        return "Using %s for %s" % (self.units, self.user.nickname())
+
