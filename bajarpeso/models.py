4b7e753cefe70a2fd8bed7cf7ed1e65a7d907e45
Rishabh Manocha <rmanocha@gmail.com>
2009-11-28 14:56:35 +0800

    Adding all the google app engine helper files and the changes that were made.

---
+++ bajarpeso/models.py
@@ -0,0 +1,19 @@
+from google.appengine.ext import db
+
+class WeightTracker(db.Model):
+    user = db.UserProperty(required = True, auto_current_user_add = True)
+    date = db.DateProperty(required = True, auto_now_add = True)
+    weight = db.FloatProperty(required = True)
+
+    def __str__(self):
+        return '%skgs on %s for %s' % (self.weight, self.date, self.user.nickname())
+
+class WeightTrackerSettings(db.Model):
+    user = db.UserProperty(required = True, auto_current_user_add = True)
+    units = db.StringProperty(required = True, choices = set(["lbs","kgs"]))
+    target_weight = db.FloatProperty(required = False)
+    target_date = db.DateProperty(required = False)
+
+    def __str__(self):
+        return "Using %s for %s" % (self.units, self.user.nickname())
+
